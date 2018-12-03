c650e5a39c35ba4b2743004414638aded573c4a8
Kevin Marschke <kmarschke@ellem.ai>
2018-12-03 17:11:42 -0600

    enabled sql messaging for pdo errors

--- lib/db.php
+++ lib/db.php
@@ -5,6 +5,7 @@ function getDB():PDO
 	if(!isset($db))
 	{
 		$db = new PDO('mysql:host=localhost;dbname=sgdb', "root", "d");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	}
 	return $db;
 }
