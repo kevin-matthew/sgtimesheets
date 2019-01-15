6fc58cdc49dfef258afbc215075de0b8052ecd09
Parker Jones <pjones@ellemsoftware.com>
2019-01-14 22:06:22 -0600

    footer change

--- run/footer.php
+++ run/footer.php
@@ -1,8 +1,5 @@
 	<footer>
-		<p><a href="/privacy">Privacy Policy</a> - <a href="/termsofservice">Return &amp; Refund Policy</a></p>
-		<p>Need Help? <a href="/support">Contact us directly</a></p>
 		<p>&copy; Ellem L.L.C. 2017</p>
-<?=var_dump($_SESSION)?>
 	</footer>
 </body>
 
