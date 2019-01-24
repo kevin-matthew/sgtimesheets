21673a60024a25827142c3298e50a14dbac676da
Abe <morrisabram97@gmail.com>
2019-01-24 15:00:45 -0600

    Fixed Header for mobile.
    - Changed mobile.scss to make the links display in block when mobile.
    - Removed the fixed height on the header.
    - Recompiled .scss files to .css files.

--- run/header.php
+++ run/header.php
@@ -2,25 +2,27 @@
 require_once 'lib/account.php';
 $a = account_session();
 $loginmsg = $a != false ? '<nav> <a href="/" style="float: left;"> Home </a>'
-	. '<a style="color:#444;"> Logged in as ' . $a->dname . '</a>' . ' <a href="/logout.php" style="float: right;">Logout</a> </nav>' : '<a href="/login.php" style="float: right;">Login</a>';
+	. '<a style="color:#444;"> Logged in as ' . $a->dname . '</a>'
+	. '<a href="/logout.php" style="float: right;">Logout</a> </nav>'
+	: '<a href="/login.php" style="float: right;">Login</a>';
 ?>
 <html lang="en-US">
 <head>
 	<link rel='icon' href='/img/favicon.ico' type='image/x-icon'/>
-	<meta name="MobileOtimized" content="width">
+	<meta name="MobileOptimized" content="width">
 	<meta name="HandheldFriendly" content="true">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 
 <body>
 	<header style="display:block;text-align:center;">
-	 <a style="color:#444;">
-	 	<div style="text-align:center"><h1>Timesheet Manager</h1>
-	 	<h2>SmartGeo Tech</h2>
-	 	</div>
-	 </a>
-	<h3><?php print $loginmsg; ?></h3>
-	<br>
+		<a style="color:#444;">
+	 		<div style="text-align:center">
+				<h1>Timesheet Manager</h1>
+	 			<h2>SmartGeo Tech</h2>
+	 		</div>
+		</a>
+		<h3 style="min-height:1px;"><?php print $loginmsg; ?></h3><br>
   </header>
